4af48d81499cec0aac36045f40e263010d8aaaa5
Nadavkt <nadavkt0103@gmail.com>
2024-01-11 09:05:56 +0200

    Blog project part 1 completed

---
+++ server/routes/main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+// Routes
+router.get("/", (req, res) => {
+
+    const locals ={
+        title: "Nadav KT Blog",
+        description: "trying to undestand how full pproject works"
+    };
+
+
+
+  res.render("index", {locals});
+});
+
+router.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+module.exports = router;
